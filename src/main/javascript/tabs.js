18b659aeddbcb19c91ad1bd8c917b20dbca3d489
Anders Nawroth <anders@neotechnology.com>
2015-12-02 00:02:05 +0100

    Tabs for language-connected content

--- src/main/javascript/tabs.js
+++ src/main/javascript/tabs.js
@@ -1,4 +1,10 @@
 function tabTheSource($content) {
+    var LANGUAGES = {
+        'csharp': 'C#',
+        'java': 'Java',
+        'javascript': 'JavaScript',
+        'python': 'Python'
+    };
     var $UL = $('<ul class="nav nav-tabs" role="tablist"/>');
     var $LI = $('<li role="presentation"/>');
     var $A = $('<a role="tab" data-toggle="tab" style="text-decoration:none;"/>');
@@ -10,8 +16,19 @@ function tabTheSource($content) {
         var title = $exampleBlock.children('div.title', this).first().text();
         var languages = [];
         var $languageBlocks = {};
-        $('div.listingblock', this).each(function () {
-            var language = $('code', this).data('lang');
+        $(this).children('div.content').children('div.listingblock,div.openblock[class*="include-with"]').each(function () {
+            var $this = $(this);
+            var language = undefined;
+            if ($this.hasClass('listingblock')) {
+                language = $('code', this).data('lang');
+            } else {
+                for (var key in LANGUAGES) {
+                    if ($this.hasClass('include-with-' + key)) {
+                        language = key;
+                        break;
+                    }
+                }
+            }
             languages.push(language);
             $languageBlocks[language] = $(this);
         });
@@ -27,6 +44,7 @@ function tabTheSource($content) {
     for (var ix = 0; ix < snippets.length; ix++) {
         var snippet = snippets[ix];
         var languages = snippet.languages;
+        languages.sort();
         var $languageBlocks = snippet.$languageBlocks;
         var $exampleBlock = snippet.$exampleBlock;
         var idBase = 'tabbed-example-' + idNum++;
@@ -45,7 +63,7 @@ function tabTheSource($content) {
             $content.addClass('tab-pane').css('position', 'relative');
             var $li = $LI.clone();
             var $a = $A.clone();
-            $a.attr('href', '#' + id).text(language).data('lang', language).on('shown.bs.tab', function (e) {
+            $a.attr('href', '#' + id).text(LANGUAGES[language]).data('lang', language).on('shown.bs.tab', function (e) {
                 var language = $(e.target).data('lang');
                 var $elements = languageEventElements[language];
                 for (var j = 0; j < $elements.length; j++) {
