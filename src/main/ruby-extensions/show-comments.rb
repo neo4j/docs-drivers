9010c136e36d09cd8824786764f0ff602bccfb4c
Anders Nawroth <anders@neotechnology.com>
2015-12-07 16:52:33 +0100

    Remove the TODO processor for now

--- src/main/ruby-extensions/show-comments.rb
+++ src/main/ruby-extensions/show-comments.rb
@@ -6,8 +6,8 @@ include Asciidoctor
 class ShowComments < Asciidoctor::Extensions::Preprocessor
   def process document, reader
     if document.attr? 'showcomments'
-      Reader.new reader.readlines.map { |l|
-        if (l.start_with? '// TODO ') || (l.start_with? '// FIXME ') || (l.start_with? '// TODO:') || (l.start_with? '// FIXME:')
+      Reader.new reader.read_lines.map { |l|
+        if (l.start_with? '// TODO') || (l.start_with? '// FIXME')
           %([comment]###{l[3..-1]}##)
         else
           l
